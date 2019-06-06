948b2777bbd4a79f75f5616f24266540b62faab2
Candice Parker <lexus2424@gmail.com>
2019-06-06 00:50:43 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :artist, :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+  
+ def self.all
+  @@all
+end
+  
+  def artist_name
+    self.artist.name
+  end
+ 
+
+end
