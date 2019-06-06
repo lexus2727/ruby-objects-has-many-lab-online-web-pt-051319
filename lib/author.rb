6eef7f107806979514a8e8f5811d2803359aca7d
Candice Parker <lexus2424@gmail.com>
2019-06-06 23:32:26 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,9 +3,20 @@ class Author
   
   def initialize(name)
     @name = name
+    @posts =[]
   end
   
   def posts
+    @posts
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.artist = self
+  end
+  
+  def add_post_by_title(title)
+    title = Post.new(title)
     
   end
 end
